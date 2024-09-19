a03d0df955a92e1c71c36db058b8a75be71ec93f
David Hustace <david@beaconstrategists.com>
2024-09-19 17:23:11 -0400

    Initial commit

---
+++ src/main/java/com/beaconstrategists/clientcaseapi/ClientApiDemoApplication.java
@@ -0,0 +1,13 @@
+package com.beaconstrategists.clientcaseapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientApiDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClientApiDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/beaconstrategists/clientcaseapi/ClientApiDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beaconstrategists.clientcaseapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientApiDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
