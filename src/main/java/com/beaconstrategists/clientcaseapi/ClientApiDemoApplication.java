337a5cd31e1a9111dbad5075fe76e3ec3bbb5f79
David Hustace <david@beaconstrategists.com>
2024-09-20 12:38:10 -0400

    - Trace logging added for debugging - Corrections made to OpenAPI specification to improve consistency and intent of the API calls - Tested component scanning in the Spring Boot App - Cleaned up pom.xml so that the Spring Doc app is correctly exposed and other unnecessary dependencies were removed.

--- src/main/java/com/beaconstrategists/clientcaseapi/ClientApiDemoApplication.java
+++ src/main/java/com/beaconstrategists/clientcaseapi/ClientApiDemoApplication.java
@@ -4,6 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+
+
+/*
+@ComponentScan(
+        basePackages = {"com.beaconstrategists"}
+)
+*/
 public class ClientApiDemoApplication {
 
     public static void main(String[] args) {
