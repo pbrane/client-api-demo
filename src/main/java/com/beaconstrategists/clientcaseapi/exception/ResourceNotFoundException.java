0a64dbe7fc52f324cda2e6167ab753073569bd36
David Hustace <david@opennms.org>
2024-09-22 10:13:30 -0400

    working versions with Spring JPA managed H2 persistence available and generated stub controllers enabled

--- src/main/java/com/beaconstrategists/clientcaseapi/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.beaconstrategists.clientcaseapi.exception;
-
-public class ResourceNotFoundException extends RuntimeException{
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
